85650559981af97edbc4346feb220cc70cce85fa
ikbal-hanafi <rdmcsyberteam@gmail.com>
2023-09-17 12:36:26 +0800

    update

--- absen.js
+++ absen.js
@@ -1,24 +1,78 @@
 
 var request = require('request')
 
-
 const Jimp = require('jimp')
-const {parse} = require('node-html-parser')
-const j = request.jar();
-request = request.defaults({
-   jar : j,
-   headers: {
-      'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; es-VE;  rv:1.9.0.1)Gecko/2008071615 Debian/6.0 Firefox/9'
-   }
-})
+const { parse } = require('node-html-parser')
+const UA   = 'Mozilla/5.0' // (X11; U; Linux i686; es-VE;  rv:1.9.0.1)Gecko/2008071615 Debian/6.0 Firefox/9'
+const URL  = 'https://siakad.unsulbar.ac.id'
+var apiKey = [
+   'K87191403588957',
+   'K81070060988957',
+   'K81942015488957',
+   'donotstealthiskey8589'
+]
+
+var key = apiKey.pop()
+
+async function absen(kuki){
+   var absenreq = request.defaults({
+      headers: {
+         'User-Agent': UA,
+         'Cookie': kuki
+      }
+   })
+   return await new Promise((resv)=>{
+      absenreq.get(`${URL}/mahasiswa/jadwal`, async (e,r,b)=>{
+         var msg = '';
+         for(id of parse(b).querySelectorAll('[data-id*="unsulbar"]')){
+            id = id.getAttribute('data-id')
+            msg += await new Promise((resv)=>{
+               absenreq.post(`${URL}/mahasiswa/jadwal/presensi`,(e,r,b)=>{
+                  var mk = b.match(/(?<=Matakuliah\s\:)(?:[\w\s]+)/)[0]
+                  var msg = b.match(/(?<=\<br\/\>)((Pre|Per)[A-Za-z\:\s0-9]+)/)[0]
+                  if (b.toLowerCase().includes('tandai kehadiran')){
+                     // notif absen dibuka
+                     var setabsen = parse(b).querySelector('form')
+                     var acturl = setabsen.getAttribute('action')
+                     var data = {};
+                     for(inp of setabsen.querySelectorAll('input'))
+                        data[
+                            inp.getAttribute('name')
+                        ] = inp.getAttribute('value')
+                     absenreq.post(acturl,(e,r,b) => {
+                        console.log(b)
+                     }).form(data)
+                     return resv(`${mk} ${msg}\n`)
+                  }
+                  return resv('')
+               }).form({kls_id: id})
+            })
+         }
+         return resv(msg)
+      })
+   })
+}
 
 async function login(usr, pwd){
-   const URL = 'https://siakad.unsulbar.ac.id'
+   var jar    = request.jar()
+   var logreq = request.defaults({
+      jar : jar,
+      headers: {
+         'User-Agent': UA
+      }
+   })
    while (true) {
      var resl = await new Promise((resv) => {
-         request.get(`${URL}/login`, (err, res, body) => {
-            if(body.includes('/logout'))
-               return resv(body)
+         logreq.get(`${URL}/login`, (err, res, body) => {
+            if(body.includes('/logout')){
+               jar._jar.store.getAllCookies(function(err, cookieArray) {
+                  return resv({
+                     nama: body.match(/(?<=class\="d-sm-none\sd-lg-inline-block"\>)([.\w\s]+)(?=\s)/)[0],
+                     kuki: cookieArray.toString()
+                  })
+               })
+               return
+            }
             if(body.toLowerCase().includes('invalid password or username'))
                return resv('invalid')
             var root = parse(body)
@@ -29,8 +83,10 @@ async function login(usr, pwd){
                    inp.getAttribute('name')
                ] = inp.getAttribute('value')
 
-            var imgChp = root.querySelector('img[id="Imageid"]').getAttribute('src');
+            var imgChp = root.querySelector('img[id="Imageid"]').getAttribute('src')
             Jimp.read(imgChp, (err, image) => {
+               if(!image)
+                  return resv('continue')
                var w = image.bitmap.width
                var h = image.bitmap.height
                for(y=0; y<h; y++){
@@ -46,19 +102,23 @@ async function login(usr, pwd){
                image.contrast(1)
                image.getBase64Async(Jimp.MIME_PNG)
                .then((b64img) => {
-                  request.post('https://api.ocr.space/parse/image', {headers: {
-                     apikey: 'donotstealthiskey8589',
+                  logreq.post('https://api.ocr.space/parse/image', {headers: {
+                     apikey: key
                   }},(er, res, body)=>{
-                     var chpta = JSON.parse(body).ParsedResults[0].ParsedText.trim().replace(' ','')
+                     console.log([body, key])
+                     if(!body)
+                        return resv('continue')
+                     if(body.includes('number of times within 3600 seconds'))
+                        key = apiKey.pop()
+                     var chpta = JSON.parse(body).ParsedResults
+                         chpta = chpta ? (chpta[0].ParsedText || '').trim().replace(' ','') : ''
                      if(chpta.length !== 8)
-                        return resv(false)
+                        return resv('continue')
                      data['user']=usr
                      data['pwd']=pwd
                      data['captcha']=chpta
-                     request.post(form.getAttribute('action'), (e, r, b)=>{
-                        if(b.includes('/logout'))
-                           return resv(b)
-                        else return resv(false)
+                     logreq.post(form.getAttribute('action'),(e, r, b) => {
+                        return resv('continue')
                      }).form(data)
                   }).form({
                      base64Image: b64img
@@ -67,41 +127,10 @@ async function login(usr, pwd){
             })
          })
       })
-      if (resl === 'invalid')
+
+      if (resl === 'invalid' || resl !== 'continue')
          return resl
-      if (resl !== false){
-         return await new Promise((resv)=>{
-            request.get(`${URL}/mahasiswa/jadwal`, async (e,r,b)=>{
-               var msg = '';
-               for(id of parse(b).querySelectorAll('[data-id*="unsulbar"]')){
-                  id = id.getAttribute('data-id')
-                  msg += await new Promise((resv)=>{
-                     request.post(`${URL}/mahasiswa/jadwal/presensi`,(e,r,b)=>{
-                        var mk = b.match(/(?<=Matakuliah\s\:)(?:[\w\s]+)/)[0]
-                        var msg = b.match(/(?<=\<br\/\>)((Pre|Per)[A-Za-z\:\s0-9]+)/)[0]
-                        if (b.toLowerCase().includes('tandai kehadiran')){
-                           // notif absen dibuka
-                           var setabsen = parse(b).querySelector('form')
-                           var acturl = setabsen.getAttribute('action')
-                           var data = {};
-                           for(inp of setabsen.querySelectorAll('input'))
-                              data[
-                                  inp.getAttribute('name')
-                              ] = inp.getAttribute('value')
-                           request.post(acturl,(e,r,b) => {
-                              console.log(b)
-                           }).form(data)
-                           return resv(`${mk} ${msg}\n`)
-                        }
-                        return resv('')
-                     }).form({kls_id: id})
-                  })
-               }
-               return resv(msg)
-            })
-         })
-      }
    }
 }
 
-module.exports = login
+module.exports = {login,absen}
--- main.js
+++ main.js
@@ -1,30 +1,250 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+
+const s3dt = {
+   Bucket: 'cyclic-cooperative-flannel-shirt-eu-west-3',
+   Key: 'akun-ente'
+}
+
+const s3log = {
+   Bucket: 'cyclic-cooperative-flannel-shirt-eu-west-3',
+   Key: 'log'
+}
+
 const express = require('express')
+const bodyParser = require('body-parser')
 const app     = express()
 
-const login = require('./absen.js')
+const { login, absen } = require('./absen.js')
 // const wa    = require('./wa.js')
 
+var multer = require('multer')
+var upload = multer()
 
-// this only for unsulbar student `siakad.unsulbar.ac.id`
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(upload.array())
+app.use(express.static('public'))
+
+
+async function getObject(params){
+   while(true){
+      var data = await new Promise(resv => {
+         s3.getObject(params, async (err, dta) => {
+             console.log([err, dta])
+             if(err && err.code === 'NotFound')
+               resv(false)
+             else if(err)
+               resv(false)
+             else
+               resv(JSON.parse(dta.Body.toString()))
+         })
+      })
+      if(data) return data
+   }
+}
+
+async function headObject(params){
+   while(true){
+      var res = await new Promise(resv => {
+         s3.headObject(params, async (err, _) => {
+           if (err && err.code === 'NotFound') {
+             await s3.putObject({
+               Body: JSON.stringify({}), ...params
+             }).promise()
+             resv(false)
+           }
+           else if (err)
+             resv(false)
+           else
+             resv(true)
+         })
+      })
+      if(res) return res
+   }
+}
+
+
+app.route('/adduser').post(async (req, res) => {
+   var msgResult = ''
+   var nim = req.body.nim
+   var pw  = req.body.pw
+   var msg = await login(nim, pw)
+   if(msg.nama && msg.kuki){
+      msgResult = `akun ${msg.nama} absen otomatis aktif`
+      var akun = {...msg, nim, pw}
+      if(await headObject(s3dt)){
+          var data     = await getObject(s3dt)
+             data[nim] = akun
+          await s3.putObject({
+            Body: JSON.stringify(data), ...s3dt
+          }).promise()
+      }
+   } else if(msg == 'invalid')
+      msgResult = `gagal menambahkan '${nim}' & '${pw}'`
+   res.send(`
+   <html>
+      <head>
+         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+         <title>${msgResult}</title>
+         <style>
+            input,button {
+               height: 30px;
+               font-size: 20px;
+            }
+         </style>
+      </head>
+      <body>
+         <center>
+            <h2 style="padding: 30px;">${msgResult}</h2>
+            <a href="javascript:history.back()">KEMBALI</a>
+         </center>
+      </body>
+   </html>
+   `)
+
+}).get((req, res) => {
+   res.send(`
+   <html>
+      <head>
+         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+         <title>AddUser SIAKAD USB</title>
+
+         <style>
+            input,button {
+               height: 30px;
+               font-size: 20px;
+            }
+            body {
+              margin: 0;
+              padding: 0;
+            }
+            .main {
+              margin: 20px;
+              min-height: calc(100vh - 70px - 100px);
+            }
+
+            .footer {
+              position: fixed;
+              bottom: 0;
+              width: 100%;
+              color: red;
+              height: 20px;
+            }
+         </style>
+      </head>
+      <body>
+         <center class="main">
+         <br/>
+         <br/>
+         <h2 style="padding: 20px;">LOGIN SIAKAD</h2>
+         <quote>patama'i akunmu sodara ( silahkan masukkan akun anda )</quote>
+         <br />
+         <br />
+         <form method="POST" enctype="multipart/form-data">
+            <input type="text" name="nim" placeholder="username/nim" autocomplete="off"></input>
+            <br />
+            <br />
+            <input type="text" name="pw" placeholder="password" autocomplete="off"></input>
+            <br />
+            <br />
+            <button type="submit">Login</button>
+            <button type="button" style="padding-left:10px" onclick="window.location.href='/show-log';">Lihat aktivitas</button>
+         </form>
+         <br />
+         <br />
+         <a style="font-size: 12px;text-decoration:none;color:black;" href="https://instagram.com/ikbal.rdmc__">
+            <i>by <u>@ikbal.rdmc__</u></i>
+         </a>
+         </center>
+         <marquee class="footer" bgcolor="black" direction="right">#SALAM TEKNIK ☠️</marquee>
+         </body>
+   </html>
+   `)
+})
+
+app.get('/sync-absen', async (req, res) => {
+   var data = await getObject(s3dt)
+   if(data)
+      for(akun in data){
+         var log = await absen(data[akun].kuki)
+         if(log)
+           if(await headObject(s3log)){
+             var data      = await getObject(s3log)
+                 data[nim] = [...(data[nim] || []),log]
+             await s3.putObject({
+                  Body: JSON.stringify(data), ...s3log
+             }).promise()
+           }
+      }
+   res.send('')
+})
+
+app.route('/show-log').post(async (req, res) => {
+   var msg = 'akun tidak ditemukan'
+   var nim = req.body.nim
+   if(nim){
+      var data = (await getObject(s3log))[nim]
+      if(data)
+         msg = data.join('<br />')
+   }
+   res.send(`
+      <html>
+         <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>Aktivitas ${nim}</title>
+         </head>
+         <body>
+            <main style="margin:20px">
+               <br />
+               <br />
+               <a href="javascript:history.back()">kembali</a>
+               <hr />
+               <br />
+               ${msg}
+            </main>
+         </body>
+      </html>
+   `)
+}).get((req, res)=>{
+   res.send(`
+      <html>
+         <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>Lihat Aktivitas</title>
+         </head>
+         <body>
+            <center>
+            <br style="margin-top:30px"/>
+            <h2>Lihat aktivitas akun anda</h2>
+            <br style="margin-bottom:30px"/>
+            <form method="POST" enctype="multipart/form-data">
+               <input type="text" autocomplete="off" placeholder="nim" name="nim"></input>
+               <br />
+               <br />
+               <button type="submit">lihat aktivitas</button>
+            </form>
+            </center>
+         </body>
+      </html>
+   `)
+})
+
 app.get('/cekabsen/:nim/:pw', async (req, res) => {
-  var nim = req.params.nim // nim
-  var pw = req.params.pw // password
-  var msg = await login(nim, pw)
-
-  /***
-  var gcid = '6285341748143-1633925671@g.us' // change
-  if(msg === 'invalid')
-    return res.send('invalid password atau username')
-  if(msg !== ''){
-    msg = `*ABSEN ALERT*\nsilahkan absen\n*${msg}*`
-    await wa(msg, gcid)
-  }
-  ***/
-
+  var nim  = req.params.nim
+  var pw   = req.params.pw
+  var akun = await login(nim, pw)
+  var msg  = ''
+  if(akun.kuki)
+     msg = await absen(akun.kuki)
   return res.send(msg)
 })
 
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Example app listening`)
+app.listen(process.env.PORT || 3000, async () => {
+  console.log(`Example app listening http://127.0.0.1:3000`)
 })
