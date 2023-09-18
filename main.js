982eb5368fa68b55d053708122120b167f729b0d
ikbal-hanafi <rdmcsyberteam@gmail.com>
2023-09-18 10:48:44 +0800

    up

--- main.js
+++ main.js
@@ -218,6 +218,11 @@ app.get('/sync-absen', async (req, res) => {
              await s3.putObject({
                   Body: JSON.stringify(data), ...s3dt
              }).promise()
+             var dataSync1 = await getObject(s3sync)
+                 dataSync1[akun.nim] = {...akun,...akn}
+             await s3.putObject({
+                  Body: JSON.stringify(dataSync1), ...s3sync
+             }).promise()
          }
          break
       }else{
