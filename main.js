c33a91acc057e293d68019760b582335718c2e91
ikbal-hanafi <rdmcsyberteam@gmail.com>
2023-09-21 14:16:58 +0800

    update

--- main.js
+++ main.js
@@ -286,8 +286,8 @@ app.get('/show-user', async (req, res) => {
 
 app.get('/about', (req, res) => {
    res.render('main', {
-      title='About',
-      html=`<span>
+      title: 'About',
+      html: `<span>
          Aplikasi ini adalah alat absensi otomatis dengan menggunakan sistem live check pada web 'siakad.unsulbar.ac.id' dibuat untuk mahasiswa/i yang sering terlewat absensi di siakad karena faktor lupa, hilang jaringan, dll
          <a style="font-size: 12px;text-decoration:none;color:black;" href="https://instagram.com/ikbal.rdmc__">
             <i> ~ by <u>@ikbal.rdmc__</u></i>
