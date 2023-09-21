a1603d707ebb96a3e8bc085e7e9a7193e5ed8be2
ikbal-hanafi <rdmcsyberteam@gmail.com>
2023-09-21 15:22:09 +0800

    update

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ app.post('/set-kelas', async (req, res) => {
    var title = ''
    var msg   = ''
    if(kelas.length !== 0){
-      var rkls = '<ul class="chck">'
+      var rkls = '<ul>'
       for(dt of kelas.map(x => JSON.parse(unescape(x)))){
          rkls += `<li>${dt.mk}</li>`
          kolas[dt.id] = dt.mk
@@ -106,7 +106,7 @@ app.post('/set-kelas', async (req, res) => {
              kls[nim]={kelas: kolas}
          await putObject(s3kls,kls)
          title = 'Selesai disimpan'
-         msg   = `Halo ${name} [ ${nim} ] kelas sudah di simpan, anda bisa perbarui dengan loging ulang<br /><br />${rkls}`
+         msg   = `<img src="/img/checklist.png" style="text-align: text;width: 150px;"></img><br />Halo ${name} [ ${nim} ] kelas sudah di simpan, anda bisa perbarui dengan loging ulang<br /><br />${rkls}`
       }
    }
    res.render('main', {
