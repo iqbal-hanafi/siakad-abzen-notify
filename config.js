3a12b3cdeeb7166130fecede9669565de99fc430
ikbal-hanafi <rdmcsyberteam@gmail.com>
2023-09-23 14:56:21 +0800

    update

---
+++ config.js
@@ -0,0 +1,36 @@
+const Bucket = 'cyclic-cooperative-flannel-shirt-eu-west-3'
+const s3dt = {
+   Key: 'akun-ente',
+   Bucket
+}
+
+const s3log = {
+   Key: 'log',
+   Bucket
+}
+
+const s3sync = {
+   Key: 'next-user',
+   Bucket
+}
+
+const s3logt = {
+   Key: 'loga-today',
+   Bucket
+}
+
+const s3kls = {
+   Key: 'kelas',
+   Bucket
+}
+
+const s3qrwa = {
+   Key: 'qr-wa',
+   Bucket
+}
+
+const nim_admin = 'D0121505'
+
+module.exports = {
+   s3dt, s3log, s3kls, s3qrwa, s3logt, s3sync, nim_admin
+}
---
+++ db.js
@@ -0,0 +1,54 @@
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
+
+async function deleteObject(params){
+   return await s3.deleteObject(params).promise()
+}
+
+async function putObject(params, body){
+   return await s3.putObject({
+      Body: JSON.stringify(body), ...params
+   }).promise()
+}
+
+async function headObject(params){
+   while(true){
+      var res = await new Promise(resv => {
+         s3.headObject(params, async (err, _) => {
+           if (err && err.code === 'NotFound') {
+             await putObject(params, {})
+             resv(false)
+           }
+           else if (err)
+             resv(false)
+           else
+             resv(true)
+         })
+      })
+      if(res) return res
+   }
+}
+
+async function getObject(params){
+   if(await headObject(params)){
+      while(true){
+         var data = await new Promise(resv => {
+            s3.getObject(params, async (err, dta) => {
+                if(err && err.code === 'NotFound')
+                  resv(false)
+                else if(err)
+                  resv(false)
+                else
+                  resv(JSON.parse(dta.Body.toString()))
+            })
+         })
+         if(data) return data
+     }
+   }
+}
+
+
+module.exports = {
+   getObject, headObject, putObject, deleteObject,
+   Bucket, s3
+}
