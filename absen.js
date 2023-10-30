3f9c78b8a68c7c5f90cc14ae89110db8f91ab6c4
ikbal-hanafi <rdmcsyberteam@gmail.com>
2023-10-30 15:44:30 +0800

    update

--- absen.js
+++ absen.js
@@ -144,7 +144,6 @@ async function login(usr, pwd){
                ] = inp.getAttribute('value')
 
             var imgChp = root.querySelector('img[id="Imageid"]').getAttribute('src')
-            console.log(imgChp)
             Jimp.read(imgChp, (err, image) => {
                if(!image)
                   return resv('continue')
@@ -166,6 +165,7 @@ async function login(usr, pwd){
                   logreq.post('https://api.ocr.space/parse/image', {headers: {
                      apikey: key
                   }},(er, res, body)=>{
+                     console.log(body)
                      if(!body)
                         return resv('continue')
                      if(body.includes('number of times within 3600 seconds'))
