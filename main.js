6654876318e800a330a9c29abc8c1de6b452b016
ikbal-hanafi <rdmcsyberteam@gmail.com>
2023-11-14 09:42:46 +0800

    update

--- main.js
+++ main.js
@@ -313,11 +313,12 @@ app.get('/', async (req, res) => {
 
 app.get('/about', (req, res) => {
    res.render('main', {
-      title: 'About',
+      title: 'Bot Siakad',
       html: `<span style="text-align: center">
-        <!---<div class="card-image">
-             <img src="https://scontent.cdninstagram.com/v/t51.2885-15/248444045_620208956055681_81260960514199161_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent.cdninstagram.com&_nc_cat=109&_nc_ohc=0KSrCT_R2vcAX9DRWr4&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MjY5MzY3NzY4ODYxNDI1MDY2NA%3D%3D.2-ccb7-5&oh=00_AfCOMd9MbtTyP8MGl0HkrF952u2a8qlbZXJJbwVSAnBuMw&oe=65572D3B&_nc_sid=10d13b" class="img-responsive">
-        </div>--->
+        <div class="card-image">
+         <!---<img src="https://scontent.cdninstagram.com/v/t51.2885-15/248444045_620208956055681_81260960514199161_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent.cdninstagram.com&_nc_cat=109&_nc_ohc=0KSrCT_R2vcAX9DRWr4&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MjY5MzY3NzY4ODYxNDI1MDY2NA%3D%3D.2-ccb7-5&oh=00_AfCOMd9MbtTyP8MGl0HkrF952u2a8qlbZXJJbwVSAnBuMw&oe=65572D3B&_nc_sid=10d13b" class="img-responsive">--->
+         <img src="img/logobs.png" class="img-responsive">
+        </div>
         <div class="card-header">
           <div class="card-subtitle">Aplikasi ini adalah alat presensi otomatis dengan menggunakan sistem live check pada web <mark>siakad.unsulbar.ac.id</mark> dibuat untuk mahasiswa/i yang sering terlewat presensi di siakad karena faktor lupa, hilang jaringan, dll</div>
         </div>
